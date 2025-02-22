a2b6b16da34548a2e2398d834f02b18594c458ef
Iosif <iosif.balog@c4media.com>
2025-02-22 17:16:28 +0200

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
+//yet another comment
+
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
